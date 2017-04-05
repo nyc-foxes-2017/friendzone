c4a62c60ff27a666dfe3cc14064e4142a9b1b96d
Jamie Rogers <james.p.rogers@gmail.com>
2017-04-05 11:30:55 -0400

    no self friending

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 060b68631112c372744be8a7c903bb48c64dc269
   has_many :posts
   has_many :likes
   has_many :liked_posts, through: :likes
@@ -11,13 +6,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
-<<<<<<< HEAD
 
-=======
-  has_many :friendships
-  has_many :posts
-  has_many :likes
->>>>>>> f2b35ed68af6738ebf47bc4e6599d1a2fc22a085
-=======
->>>>>>> 060b68631112c372744be8a7c903bb48c64dc269
+  def request(user)
+    Friendship.new(requester: self.id, requestee: user.id)
+  end
+
 end
