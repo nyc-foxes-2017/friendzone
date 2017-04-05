3d88556f3f909f1254ac1ec1987fe1019b516ceb
Jamie Rogers <james.p.rogers@gmail.com>
2017-04-05 16:38:58 -0400

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :users, except: [:index]
 resources :posts
-resources :friendship
+resources :friendships
 root 'posts#index'
 resources :users, :only => [:show]
 end
